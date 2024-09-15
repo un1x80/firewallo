85daff3d4fad9074ca712cbd95d96599d28e63d8
un1x80 <fioritim@gmail.com>
2024-09-15 23:07:19 +0200

    First Commit 24.9.1

--- etc/firewallo/bin/magic_nat_postrouting.sh
+++ etc/firewallo/bin/magic_nat_postrouting.sh
@@ -20,6 +20,39 @@ handle_error() {
     echo "$INVALID_SELECTION_MSG"
 }
 
+# Funzione per validare un indirizzo IP con maschera
+validate_ip_mask() {
+    local ip_mask="$1"
+    if [[ "$ip_mask" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
+        return 0  # valido
+    else
+        handle_error "$INVALID_SADDR"
+        return 1  # non valido
+    fi
+}
+
+# Funzione per validare la porta
+validate_port() {
+    local port="$1"
+    if [[ "$port" =~ ^[0-9]+$ ]] || [[ "$port" == "any" ]] || [[ -z "$port" ]]; then
+        return 0  # valido
+    else
+        handle_error "$INVALID_DPORT"
+        return 1  # non valido
+    fi
+}
+
+# Funzione per validare l'interfaccia di rete
+validate_oif() {
+    local oif="$1"
+    if [[ "$oif" =~ ^[a-zA-Z0-9]+$ ]]; then
+        return 0  # valido
+    else
+        handle_error "$INVALID_SELECTION_MSG"
+        return 1  # non valido
+    fi
+}
+
 # Funzione per configurare le regole POSTROUTING
 configure_postrouting() {
     local srcip_mask="$1"
@@ -83,8 +116,17 @@ ask_for_parameters() {
     echo "$POSTROUTING_CONFIG_PROMPT"
     echo ""
 
-    read -e -p "$SRCIP_MASK_PROMPT" srcip_mask
-    read -e -p "$OIF_PROMPT" oif
+    # Chiedi l'indirizzo IP di origine e verifica
+    while true; do
+        read -e -p "$SRCIP_MASK_PROMPT" srcip_mask
+        validate_ip_mask "$srcip_mask" && break
+    done
+
+    # Chiedi l'interfaccia di uscita e verifica
+    while true; do
+        read -e -p "$OIF_PROMPT" oif
+        validate_oif "$oif" && break
+    done
 
     # Mostra il menu per la scelta del tipo di masquerading
     while true; do
@@ -109,17 +151,17 @@ ask_for_parameters() {
         esac
     done
 
-    read -e -p "$PORT_PROMPT" dport
+    # Chiedi la porta e verifica
+    while true; do
+        read -e -p "$PORT_PROMPT" dport
+        validate_port "$dport" && break
+    done
 
+    # Se il tipo è SNAT, chiedi l'indirizzo IP di destinazione
     if [ "$type" == "SNAT" ]; then
         while true; do
             read -e -p "$SNAT_SOURCE_IP_PROMPT" to_source_ip_mask
-            # Verifica la validità dell'indirizzo IP e maschera
-            if [[ "$to_source_ip_mask" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/[0-9]+$ ]]; then
-                break
-            else
-                handle_error "$INVALID_SOURCIP_MASK_FORMAT"
-            fi
+            validate_ip_mask "$to_source_ip_mask" && break
         done
     else
         to_source_ip_mask=""
@@ -134,4 +176,3 @@ load_translations
 
 # Eseguire la funzione per chiedere i parametri
 ask_for_parameters
-
