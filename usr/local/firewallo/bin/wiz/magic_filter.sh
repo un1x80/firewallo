9fa66acf6419ffc054df27bf597114cb141183c4
un1x80 <fioritim@gmail.com>
2024-09-23 23:08:28 +0200

    First Commit 24.9.1

--- usr/local/firewallo/bin/wiz/magic_filter.sh
+++ usr/local/firewallo/bin/wiz/magic_filter.sh
@@ -1,125 +1,7 @@
 #!/bin/bash
 
-DIRCONF="/etc/firewallo"
-source $DIRCONF/firewallo.conf
-
-# Carica il file di traduzione
-load_translations() {
-    local lang_file="$DIRCONF/lang/firewallo.lang.$LANG"
-    if [[ -f "$lang_file" ]]; then
-        source "$lang_file"
-    else
-        echo "LANG_NOT_FOUND"
-        exit 1
-    fi
-}
-
-handle_error() {
-    echo "$1" 1>&2
-    echo "$INVALID_SELECTION_MSG"
-}
-
-
-# Funzione per visualizzare le catene disponibili e permettere la selezione
-select_chain() {
-    echo "$SELECT_CHAIN_PROMPT"
-    CHAINS=(
-        fw2fw	fw2lan	fw2wan	fw2vpns	    fw2dmz \
-        lan2fw	lan2lan	lan2wan	lan2vpns	lan2dmz \
-        wan2fw	wan2lan	wan2wan	wan2vpns	wan2dmz \
-        vpns2fw	vpns2lan	vpns2wan	vpns2vpns	vpns2dmz \
-        dmz2fw	dmz2lan	dmz2wan	dmz2vpns	dmz2dmz \
-        exit
-    )
-
-    # Mostra le catene all'utente
-    select chain in "${CHAINS[@]}"; do
-        if [[ " ${CHAINS[@]} " =~ " $chain " ]]; then
-            echo "$(printf "$CHAIN_SELECTED_MSG" "$chain")"
-            CHAIN_SELECTED=$chain
-            if [ "$chain" = "exit" ] ; then
-                exit    
-            else
-                break
-            fi
-        else
-            echo "$INVALID_SELECTION_MSG"
-        fi
-    done
-}
-
-# Funzione per controllare la validità dell'indirizzo IP
-validate_ip() {
-    local ip="$1"
-    if [[ "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(/[0-9]{1,2})?$ ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Funzione per controllare la validità del protocollo
-validate_protocol() {
-    local protocol="$1"
-    if [[ "$protocol" == "tcp" || "$protocol" == "udp" ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Funzione per controllare la validità della porta
-validate_port() {
-    local port="$1"
-    if [[ "$port" == "any" || "$port" =~ ^[0-9]+$ && "$port" -ge 1 && "$port" -le 65535 ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Funzione per adattare i valori 'any' e 'range' per iptables
-parse_port_range() {
-    local port_range="$1"
-    if [[ "$port_range" == "any" ]]; then
-        echo "1:65535"
-    elif [[ "$port_range" =~ ^([0-9]+):([0-9]+)$ ]]; then
-        echo "$port_range"
-    else
-        echo "$port_range"
-    fi
-}
-
-# Funzione per adattare i valori 'any' e 'range' per nftables
-parse_port_range_nft() {
-    local port_range="$1"
-    if [[ "$port_range" == "any" ]]; then
-        echo "1-65535"
-    elif [[ "$port_range" =~ ^([0-9]+):([0-9]+)$ ]]; then
-        echo "$port_range"
-    else
-        echo "$port_range"
-    fi
-}
-
-# Funzione per tradurre le azioni in nftables
-translate_action() {
-    case "$1" in
-        ACCEPT)
-            echo "counter accept"
-            ;;
-        DROP)
-            echo "counter drop"
-            ;;
-        REJECT)
-            echo "counter reject"
-            ;;
-        *)
-            echo "Invalid action"
-            exit 1
-            ;;
-    esac
-}
+#Load library
+source /usr/local/firewallo/lib/lib-fwallo.sh
 
 # Carica le traduzioni
 load_translations
@@ -207,8 +89,8 @@ while true; do
 done
 if [ "$IPT" != "" ] ; then
     # Adatta le porte per iptables
-    SRC_PORT_OPTION_IPT=$(parse_port_range "$SRC_PORT")
-    DST_PORT_OPTION_IPT=$(parse_port_range "$DST_PORT")
+    SRC_PORT_OPTION_IPT=$(parse_port_range_ipt "$SRC_PORT")
+    DST_PORT_OPTION_IPT=$(parse_port_range_ipt "$DST_PORT")
     # Aggiungi la regola in iptables
     iptables_cmd="iptables -t filter -A $CHAIN_SELECTED -p $PROTOCOL -s $SRC_ADDR --sport $SRC_PORT_OPTION_IPT -d $DST_ADDR --dport $DST_PORT_OPTION_IPT -j $ACTION"
     echo "$IPT_RULE_MSG" ; echo "$iptables_cmd"
