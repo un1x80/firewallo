f8980cd45b29f4f285c4b1af5c1ab78294811908
un1x80 <fioritim@gmail.com>
2024-10-07 01:26:55 +0200

    First Commit 24.9.1

---
+++ usr/local/firewallo/bin/wiz/magic_dpi.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Carica la libreria
+source /usr/local/firewallo/lib/lib-wiz.sh
+
+# Carica le traduzioni
+load_translations
+
+# Imposta la catena fissa
+CHAIN_SELECTED="dpi"
+
+# Chiedi i dettagli uno per uno all'utente con controlli
+while true; do
+    read -e -p "$SOURCE_ADDR_PROMPT" SRC_ADDR
+    if validate_ip_mask "$SRC_ADDR"; then
+        break
+    fi
+done
+
+# Mostra il menu per la scelta del protocollo (tcp o udp)
+while true; do
+    echo ""
+    echo "$PROTOCOL_PROMPT"
+    echo "$TCP_OPTION"
+    echo "$UDP_OPTION"
+    read -e -p "$CHOICE_PROMPT" protocol_choice
+
+    case $protocol_choice in
+        1)
+            PROTOCOL="tcp"
+            break
+            ;;
+        2)
+            PROTOCOL="udp"
+            break
+            ;;
+        *)
+            handle_error "$INVALID_PROTOCOL"
+            ;;
+    esac
+done
+
+# Chiede la porta sorgente, opzionale (default "any")
+while true; do
+    read -e -p "$SRC_PORT_PROMPT" SRC_PORT
+    SRC_PORT=${SRC_PORT:-any}
+    if validate_port "$SRC_PORT"; then
+        break
+    else
+        echo "$INVALID_SPORT"
+    fi
+done
+
+# Chiede l'indirizzo di destinazione
+while true; do
+    read -e -p "$DST_ADDR_PROMPT" DST_ADDR
+    if validate_ip_mask "$DST_ADDR"; then
+        break
+    fi
+done
+
+# Chiede la porta di destinazione, opzionale (default "any")
+while true; do
+    read -e -p "$DST_PORT_PROMPT" DST_PORT
+    DST_PORT=${DST_PORT:-any}
+    if validate_port "$DST_PORT"; then
+        break
+    else
+        echo "$INVALID_DPORT"
+    fi
+done
+
+# Chiede il commento per la regola
+while true; do
+    read -e -p "$COMMENT_PROMPT" comment
+    comment=$(echo $comment | tr -s " " "_")
+    if [[ "$comment" =~ ^[a-zA-Z0-9_]+$ ]]; then
+        break
+    else
+        handle_error "$INVALID_COMMENT_FORMAT"
+    fi
+done
+
+# Aggiungi la regola con nftables per inserire il traffico nella queue 0
+if [ "$NFT" != "" ]; then
+    # Adatta le porte per nftables
+    SRC_PORT_OPTION_NFT=$(parse_port_range_nft "$SRC_PORT")
+    DST_PORT_OPTION_NFT=$(parse_port_range_nft "$DST_PORT")
+    
+    # Costruisci il comando nftables per aggiungere la regola nella catena dpi
+    nft_cmd="nft \"add rule ip filter $CHAIN_SELECTED ip saddr $SRC_ADDR ip daddr $DST_ADDR $PROTOCOL sport $SRC_PORT_OPTION_NFT $PROTOCOL dport $DST_PORT_OPTION_NFT queue num 0 comment \\\"$comment\\\"\""
+    
+    echo "$NFT_RULE_MSG"; echo "$nft_cmd"
+    
+    # Aggiungi la regola al file di configurazione, posizionandola in fondo
+    echo "
+#COMMENT:$comment
+$nft_cmd" >> $DIRCONF/filter/$CHAIN_SELECTED
+
+    echo "PRESS ENTER TO CONTINUE..." ; read ENTER
+else
+    echo "$INT_ERROR_MSG"
+    echo "PRESS ENTER TO CONTINUE..." ; read ENTER
+fi
---
+++ usr/local/firewallo/bin/wiz/magic_suricata.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# Controlla se il file esiste
+if [ -f /etc/firewallo/.sid ]; then
+    # Leggi il valore attuale da .sid e incrementalo di uno
+    SID=$(( $(cat /etc/firewallo/.sid) + 1 ))
+else
+    # Se il file non esiste, inizializza SID a 1
+    SID=1
+fi
+
+# Scrivi il nuovo valore nel file .sid
+echo $SID > /etc/firewallo/.sid
+# Array con tutti i protocolli supportati da Suricata
+protocols=(
+    "BitTorrent" "Facebook" "Skype" "WhatsApp" "YouTube"
+    "Netflix" "Tor" "SSH" "HTTPS" "HTTP" "DNS" "Dropbox"
+    "Google Drive" "OneDrive" "Steam" "PlayStation Network"
+    "Xbox Live" "Zoom" "Microsoft Teams" "Instagram" "Twitter"
+    "Snapchat" "Telegram" "TikTok" "Amazon Prime Video" "Spotify"
+    "Apple Music" "Discord" "Gmail" "Office 365" "Slack" "Bitcoin"
+    "Ethereum" "OpenVPN" "IPsec" "WireGuard" "SMTP" "IMAP" "POP3"
+)
+
+# Funzione per controllare se Suricata è attivo
+check_suricata() {
+    if ! systemctl is-active --quiet suricata; then
+        echo "Il servizio Suricata non è attivo. Avviarlo con: sudo systemctl start suricata"
+        exit 1
+    fi
+}
+
+# Funzione per mostrare i protocolli supportati
+show_protocols() {
+    echo "Scegli i protocolli da bloccare (puoi selezionarne più di uno separando i numeri con uno spazio):"
+    for i in "${!protocols[@]}"; do
+        echo "$((i + 1))) ${protocols[$i]}"
+    done
+    echo -n "Inserisci i numeri corrispondenti ai protocolli desiderati: "
+    read -a protocol_choices
+}
+
+# Funzione per ottenere i protocolli selezionati
+get_protocols() {
+    selected_protocols=()
+    for choice in "${protocol_choices[@]}"; do
+        if ((choice > 0 && choice <= ${#protocols[@]})); then
+            selected_protocols+=("${protocols[$((choice - 1))]}")
+        else
+            echo "Scelta non valida: $choice. Ignorato."
+        fi
+    done
+}
+
+# Funzione per creare le regole di Suricata
+create_rules() {
+    for protocol in "${selected_protocols[@]}"; do
+        echo "Aggiungendo regola per bloccare il traffico del protocollo $protocol..."
+	protocol=$(echo "$protocol" | tr '[:upper:]' '[:lower:]'| tr -d " ")
+
+        # Aggiungi la regola al file delle regole di Suricata
+        rule="drop ip any any -> any any (msg:\"Blocco traffico $protocol\"; app-layer-protocol:$protocol; sid:$SID; rev:1; classtype:policy-violation;)"
+        echo "$rule" | sudo tee -a /etc/suricata/rules/block.rules > /dev/null
+    done
+
+    echo "Regole aggiunte con successo al file delle regole."
+}
+
+# Funzione per riavviare Suricata
+restart_suricata() {
+    echo "Riavvio di Suricata per applicare le modifiche..."
+    sudo systemctl restart suricata
+    
+    if [ $? -eq 0 ]; then
+        echo "Suricata riavviato con successo."
+    else
+        echo "Errore durante il riavvio di Suricata."
+    fi
+}
+
+# Funzione per mostrare le regole attuali di Suricata
+show_current_rules() {
+    echo "Regole attuali nel file block.rules:"
+    sudo cat /etc/suricata/rules/block.rules
+}
+
+# Main script
+clear
+echo "---------------------------------------------"
+echo "Wizard per la configurazione di Suricata"
+echo "---------------------------------------------"
+
+# Verifica se Suricata è attivo
+check_suricata
+
+# Mostra le regole correnti
+show_current_rules
+
+# Chiedi all'utente di scegliere i protocolli da bloccare
+show_protocols
+get_protocols
+
+# Crea le regole di blocco per i protocolli scelti
+create_rules
+
+# Riavvia Suricata per applicare le modifiche
+restart_suricata
+
+# Mostra le regole aggiornate
+show_current_rules
+
+echo "Configurazione di Suricata completata con successo!"
