983946012ca194de9105f9026f91ceb6a4f9e86a
un1x80 <fioritim@gmail.com>
2024-10-17 12:36:28 +0200

    Rules debian

--- usr/local/firewallo/bin/filter/old/controllo_list.sh
+++
@@ -1,4 +0,0 @@
-lista=$(ls)
-for i in $lista ; do printf $i ; cat $i | grep $i | wc -l ; done
-for i in $lista ; do printf $i ; cat $i | grep tcp | wc -l ; done
-for i in $lista ; do printf $i ; cat $i | grep udp | wc -l ; done
